4a9986304a9361b08f429ec0209c9f6f1c59115c
Jonas Kuball <jonas.kuball@lector.ai>
2025-12-26 15:30:01 +0100

    ✨ add rust_analyzer

--- modules/lsp.nix
+++ modules/lsp.nix
@@ -18,6 +18,14 @@ in
       protols.enable = true;
       pyright.enable = true;
       ruff.enable = true;
+      rust_analyzer = {
+        enable = true;
+        # In my not so far reaching experience with rust, rust-analyzer
+        # should mostly be synced with the toolchains used in the
+        # project. So, if present, the rust-analyzer of a devshell
+        # should always be used before the bundled one.
+        packageFallback = true;
+      };
       yamlls.enable = true;
     };
   };
